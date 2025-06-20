e0ae37a8212ea98f721a3d40da3b37ef2d802b12
hendrixaguirre <hendrixag@uamv.edu.ni>
2025-06-20 10:20:22 -0600

    Delete ListaCompras.py

--- ListaCompras.py
+++
@@ -1,8 +0,0 @@
-# Programa que genera una lista de compras
-with open("compras.txt","w",encoding="utf-8") as archivo:
-    while True:
-        prod=input("Ingrese el nombre del producto (o escribe 'fin' para terminar):")
-        if prod.lower() == "fin":
-            break
-        archivo.write(prod + "\n")
-print ("La lista de compras ha sido guardada en 'compras.txt!'1")
